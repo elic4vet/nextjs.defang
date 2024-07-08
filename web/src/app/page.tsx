5f86a5919fe9cea559acbc48199664bcea09518c
Elisabeth <elise.doisneau@gmail.com>
2024-07-08 19:05:41 +0200

    Initial commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className='container'>
+      <p> Hello World from Page ! </p>
+    </div>
+  );
+};
+
+export default Page;
